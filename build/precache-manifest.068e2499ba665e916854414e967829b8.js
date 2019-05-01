40c8a98802f452e5a16288101704d09d6a996a42
Stanislav Kaisin <8050361@gmail.com>
2019-05-01 17:24:17 +0300

    added build folder in hmw1

---
+++ build/precache-manifest.068e2499ba665e916854414e967829b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "925e7aa25eee74f52780c982a0392478",
+    "url": "/static/media/logo.925e7aa2.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f5540e154fa146ec69cf",
+    "url": "/static/js/main.e40b4463.chunk.js"
+  },
+  {
+    "revision": "75c573dc01660ca363ab",
+    "url": "/static/js/2.bfb993fe.chunk.js"
+  },
+  {
+    "revision": "f5540e154fa146ec69cf",
+    "url": "/static/css/main.49fd1cb0.chunk.css"
+  },
+  {
+    "revision": "526f42959b8b98c37481aa66dbfb02b3",
+    "url": "/index.html"
+  }
+];
